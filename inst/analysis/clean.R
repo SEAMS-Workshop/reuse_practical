386d61ac5b8c7037e9506a4a6634432f8bb1d1c0
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2023-03-01 08:13:37 +0000

    update clean to use package code

--- inst/analysis/clean.R
+++ inst/analysis/clean.R
@@ -1,22 +1,22 @@
-# setup environment
-# this is still a bit dodgy - shouldn't install things for people
-if (!require(data.table)) install.packages('data.table')
-# note that clearing the environment, setting paths, etc
-# no longer happens - that's all managed outside the script
+require(seamsCFR)
 
 # fetch the arguments from the command line, other than the script name
-.args <- commandArgs(trailingOnly = TRUE)
+.args <- if (interactive()) c(
+  "data/raw/owid-covid-data.csv"
+) else commandArgs(trailingOnly = TRUE)
 
 # which are source data and target output figure
-d <- fread(.args[1])
+d <- data.table::fread(.args[1])[
+  order(date), .(date, cases = new_cases_smoothed, deaths = new_deaths_smoothed), by=iso_code
+]
 tarfile <- tail(.args, 1)
 # we use `tail()` here, because we have adopted a pattern where the last
 # argument is always the target file. now, if we add additional arguments,
 # we don't have to change picking out the target file
 
 # convert date field, compute the crude CFR
-d$date = as.Date(d$date)
-d$crude_cfr = d$new_deaths_smoothed_per_million/d$new_cases_smoothed_per_million
+
+d <- seamsCFR::lagged_cfr(d, seq(0, 21, 7))
 
 # and now we just save the data in the R native format
 # plotting will happen in a different script
